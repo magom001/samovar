843eac9f59ec0f7b67016e627664dff0e765703f
Arkady Magomedov <amagomedov@microsoft.com>
2023-11-20 22:06:22 +0100

    Foundation of the personal data form

---
+++ apps/mini-app/src/features/PersonalDataForm/PersonalDataForm.tsx
@@ -0,0 +1,71 @@
+import { Controller, useForm } from "react-hook-form";
+import type { AxiosResponse } from "axios";
+import { useMutation, useQuery } from "react-query";
+import { TextField } from "@samovar/ui/TextField";
+import { Button } from "@samovar/ui/Button";
+import { authenticatedHttpClient } from "../../services/http-client";
+
+// TODO: refactor all models
+interface WhoAmIData {
+  data: PersonalData | null;
+}
+
+interface PersonalData {
+  firstName?: string;
+  lastName?: string;
+  avatarUrl?: string;
+}
+
+function getWhoAmI(): Promise<AxiosResponse<WhoAmIData>> {
+  return authenticatedHttpClient.get("api/v1/auth/whoami");
+}
+
+export function PersonalDataForm() {
+  const result = useQuery("whoami", getWhoAmI, { suspense: true });
+
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { isDirty },
+  } = useForm<PersonalData>({
+    defaultValues: result.data?.data?.data || {},
+  });
+
+  const mutation = useMutation({
+    mutationFn: (data: PersonalData) => {
+      return authenticatedHttpClient.put<WhoAmIData>("api/v1/user/data", data);
+    },
+    onSuccess: ({ data: { data } }) => {
+      console.log("on mutation success", data);
+      reset(data ?? {});
+    },
+  });
+
+  const onSubmit = (data: PersonalData) => {
+    console.log("data", data);
+    mutation.mutate(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        control={control}
+        name="firstName"
+        render={({ field }) => (
+          <TextField {...field} fullWidth margin="dense" size="small" />
+        )}
+      />
+      <Controller
+        control={control}
+        name="lastName"
+        render={({ field }) => (
+          <TextField {...field} fullWidth margin="dense" size="small" />
+        )}
+      />
+      <Button disabled={!isDirty} type="submit">
+        Save
+      </Button>
+    </form>
+  );
+}
--- apps/mini-app/src/routes/Profile/Profile.tsx
+++ apps/mini-app/src/routes/Profile/Profile.tsx
@@ -1,89 +1,19 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@samovar/ui/Button';
 import { Dialog, DialogContent, DialogTitle } from '@samovar/ui/Dialog';
-import { TextField } from '@samovar/ui/TextField';
-import { styled } from '@samovar/ui/styles';
 import { Suspense, lazy, useState } from 'react';
-import type { SubmitHandler } from 'react-hook-form';
-import { Controller, useForm } from 'react-hook-form';
-import { z } from 'zod';
 import { IconButton } from '@samovar/ui/IconButton';
 import { Icon } from '@samovar/ui/Icon';
+import { Box } from '@samovar/ui/Box';
+import { PersonalDataForm } from '../../features/PersonalDataForm';
 
 const LazyProfileForm = lazy(() => import('../../features/ProfileForm'));
 
-interface Inputs {
-  firstName: string;
-  lastName: string;
-}
-
-const schema = z
-  .object({
-    firstName: z.string({ required_error: 'First name is required' }).trim().min(3),
-    lastName: z.string().min(3).max(10),
-  })
-  .required();
-
-const defaultValues: Inputs = {
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const Form = styled.form`
-  padding: 32px;
-`;
-
 export function Component() {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<Inputs>({
-    defaultValues,
-    resolver: zodResolver(schema),
-  });
-
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  // const [activeStep, setActiveStep] = useState(0);
-  // const moveToNextStep = () => {
-  //   setActiveStep((prev) => prev + 1);
-  // };
-  // const moveToPreviousStep = () => {
-  //   setActiveStep((prev) => prev - 1);
-  // };
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log('submit', data);
-  };
-
   return (
-    <>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="firstName"
-          render={({ field }) => (
-            <TextField
-              label="First name"
-              {...field}
-              error={Boolean(errors?.firstName?.message)}
-              fullWidth
-              helperText={errors?.firstName?.message ?? ' '}
-              margin="dense"
-              variant="standard"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="lastName"
-          render={({ field }) => (
-            <TextField fullWidth label="Last name" {...field} helperText={' '} margin="dense" variant="standard" />
-          )}
-        />
-        <input type="submit" />
-      </Form>
+    <Box p={1}>
+      <PersonalDataForm />
       <div>
         <h4>Profiles:</h4>
         <Button
@@ -112,7 +42,7 @@ export function Component() {
           </DialogContent>
         </Dialog>
       </Suspense>
-    </>
+    </Box>
   );
 }
 
