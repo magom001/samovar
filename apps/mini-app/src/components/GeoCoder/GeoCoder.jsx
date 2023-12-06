b2272823299d34b5ce1a406eab3c742f44c490d7
Sergey Akopkokhyants <sergeyak@microsoft.com>
2023-12-06 12:06:28 +0100

    Fix showing the duplicate buttons in GeoCoder

--- apps/mini-app/src/components/GeoCoder/GeoCoder.jsx
+++ apps/mini-app/src/components/GeoCoder/GeoCoder.jsx
@@ -5,13 +5,21 @@ import icon from './MarkerIcon';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
 
-export function GeoCoder() {
+function removeAllMarkers(map) {
+  map.eachLayer(layer => {
+    if (layer instanceof Marker) {
+      layer.remove();
+    }
+  });
+}
+
+export function GeoCoder({onChange}) {
   const map = useMap();
 
   useEffect(() => {
     const geocoder = Control.Geocoder.nominatim();
 
-    Control.geocoder({
+    const geo = Control.geocoder({
       position: 'topright',
       placeholder: 'Search...',
       defaultMarkGeocode: false,
@@ -20,20 +28,25 @@ export function GeoCoder() {
     })
     .on('markgeocode', (e) => {
       // Clear all markers
-      map.eachLayer(layer => {
-        if (layer instanceof Marker) {
-          layer.remove();
-        }
-      });
+      removeAllMarkers(map);
       // Get position
       const latlng = e.geocode.center;
       // Create mark
       marker(latlng, { icon }).addTo(map).bindPopup(e.geocode.name).openPopup();
       // Fit it in bounds of the map
       map.fitBounds(e.geocode.bbox);
+      // Call listeners ouside about selection
+      onChange && onChange([latlng.lat, latlng.lng]);
     })
     .addTo(map);
-  }, [map]);
+
+    return () => {
+      // Clear all markers
+      removeAllMarkers(map);
+      // Remove geocoder from map
+      geo.remove();
+    };
+  }, [map, onChange]);
 
   return null;
 }
